88356e66768324a17898d68eac4628f67ab777aa
Piotr Lasota <lasota.piotr1@gmail.com>
2017-12-11 21:48:11 +0100

    Add images

--- App.js
+++ App.js
@@ -1,34 +1,80 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  Alert,
+  ScrollView,
+} from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showText: true,
+      text: '',
     };
-
-    setInterval(() => {
-      this.setState(prevState => ({
-        showText: !prevState.showText,
-      }));
-    }, 1000);
   }
-
   render() {
     const pic = {
       uri:
         'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
     };
     return (
-      <View style={styles.container}>
-        <Image source={pic} style={{ width: '100%', height: 200 }} />
-        {this.state.showText && (
-          <Text style={styles.explicited}>
-            Banany App
-          </Text>
-        )}
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <View
+            style={{
+              width: '100%',
+              flexDirection: 'row',
+              justifyContent: 'center',
+            }}
+          >
+            <TextInput
+              style={{
+                width: '60%',
+                height: 40,
+                paddingLeft: 30,
+                paddingRight: 30,
+              }}
+              placeholder="Search tag here"
+              onChangeText={text => {
+                this.setState({ text });
+              }}
+            />
+            <Button
+              onPress={() => {
+                Alert.alert(`You tapped the button!, ${this.state.text}`);
+              }}
+              title="Press Me"
+            />
+          </View>
+          <View
+            style={{ width: '100%', height: 500, justifyContent: 'center' }}
+          >
+            <Image
+              source={pic}
+              style={{ width: '100%', height: 400, marginBottom: 20 }}
+            />
+            <Text style={{ fontSize: 20, textAlign: 'center' }}>
+              {this.state.text}
+            </Text>
+          </View>
+          <View
+            style={{ width: '100%', height: 500, justifyContent: 'center' }}
+          >
+            <Image
+              source={pic}
+              style={{ width: '100%', height: 400, marginBottom: 20 }}
+            />
+            <Text style={{ fontSize: 20, textAlign: 'center' }}>
+              {this.state.text}
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -39,9 +85,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
+    paddingTop: 100,
   },
-  explicited: {
-    color: 'red',
-    fontSize: 24,
-  }
 });
