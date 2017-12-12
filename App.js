3a2a93cb1f8ec7d1ec7a8d1db0da9c0ecb6a9f5e
Piotr Lasota <lasota.piotr1@gmail.com>
2017-12-12 21:56:20 +0100

    Add flat list

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import {
   Button,
   Alert,
   ScrollView,
+  FlatList
 } from 'react-native';
 
 export default class App extends React.Component {
@@ -16,20 +17,35 @@ export default class App extends React.Component {
     this.state = {
       text: '',
     };
+
+    this.data = [
+      {
+        name: 'Banany',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
+      },
+      {
+        name: 'Jabłka',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg',
+      },
+      {
+        name: 'Truskawki',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Truskawki.jpg',
+      },
+    ]
   }
   render() {
-    const pic = {
-      uri:
-        'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
-    };
     return (
       <ScrollView>
         <View style={styles.container}>
+          <Text style={{minHeight: 30, fontSize: 20, marginBottom: 10, textAlign: 'center'}}>
+            {this.state.text}
+          </Text>
           <View
             style={{
               width: '100%',
               flexDirection: 'row',
               justifyContent: 'center',
+              paddingBottom: 20,
             }}
           >
             <TextInput
@@ -46,33 +62,32 @@ export default class App extends React.Component {
             />
             <Button
               onPress={() => {
-                Alert.alert(`You tapped the button!, ${this.state.text}`);
+                Alert.alert(`You typed: ${this.state.text}`);
               }}
               title="Press Me"
             />
           </View>
-          <View
-            style={{ width: '100%', height: 500, justifyContent: 'center' }}
-          >
-            <Image
-              source={pic}
-              style={{ width: '100%', height: 400, marginBottom: 20 }}
+          <View stle={{ width: '100%'}}>
+            <FlatList
+              data={this.data}
+              renderItem={({item}) => (
+                <View
+                  style={{ width: '100%', height: 500}}
+                >
+                  <Image
+                    source={{uri: item.url}}
+                    style={{ width: 500, height: 400, marginBottom: 20 }}
+                  />
+                  <Text style={{ fontSize: 20, textAlign: 'center', width: '100%' }}>
+                    {item.name}
+                  </Text>
+                </View>
+              )}
+              keyExtractor={(item, index) => index}
             />
-            <Text style={{ fontSize: 20, textAlign: 'center' }}>
-              {this.state.text}
-            </Text>
           </View>
-          <View
-            style={{ width: '100%', height: 500, justifyContent: 'center' }}
-          >
-            <Image
-              source={pic}
-              style={{ width: '100%', height: 400, marginBottom: 20 }}
-            />
-            <Text style={{ fontSize: 20, textAlign: 'center' }}>
-              {this.state.text}
-            </Text>
-          </View>
+
+
         </View>
       </ScrollView>
     );
